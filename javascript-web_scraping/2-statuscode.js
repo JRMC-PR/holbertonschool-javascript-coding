3ae88a9cc8d1533c9681fcc6ebcd72f77ec57aca
JRMC-PR <jesus.rafael.mendez.cruz@gmail.com>
2024-01-23 12:09:46 -0400

    Update task 2

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -1,7 +1,11 @@
 #!/usr/bin/node
 const https = require('https');
+const http = require('http');
 
-https.get(process.argv[2], (res) => {
+const url = process.argv[2];
+protocol = url.startsWith('https') ? https : http;
+
+protocol.get(url, (res) => {
   console.log(`code: ${res.statusCode}`);
   process.exit();
 }).on('error', (res) => {
