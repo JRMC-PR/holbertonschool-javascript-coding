446a1607cc6e264397285b7a7af0c06553c3fa12
JRMC-PR <jesus.rafael.mendez.cruz@gmail.com>
2024-01-23 15:39:43 -0400

    Adds task 6

---
+++ javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,26 @@
+#!/usr/bin/node
+const request = require('request');
+const url = process.argv[2];
+
+request(url, (error, response, body) => {
+  if (error) {
+    console.error('Error:', error);
+    return;
+  }
+
+  const tasks = JSON.parse(body);
+  const completedTasks = {};
+
+  tasks.forEach(task => {
+    if (task.completed) {
+      if (completedTasks[task.userId]) {
+        completedTasks[task.userId]++;
+      } else {
+        completedTasks[task.userId] = 1;
+      }
+    }
+  });
+
+  const output = JSON.stringify(completedTasks, null, 2);
+  console.log(output.trim());
+});
